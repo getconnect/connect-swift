74425346ca15bc90fddfd0beafabfddbff03567e
Adam Gilmore <adam@Adams-Mac.local>
2016-03-01 14:49:18 +1000

    Release 0.0.12

--- Example/ConnectSwift/ViewController.swift
+++ Example/ConnectSwift/ViewController.swift
@@ -13,25 +13,41 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        let connect = Connect(
-            projectId: "559de2011f2ddd1860bd29fb",
-            apiKey: "F72EE2F564DBE33DF66171A775CD7B4E-7EE5844C84B9E3F88F87FBD0421B79063CB13858C93C4E0ED1F346522057283D8CC22C8FEDE3E8E088CD91B9BDAA25BE2AF8A3A5320C87282EB96FA98E8E8369")
         
-        connect.query("orders")
-            .select(["test": .Count])
-            .timeframe(.LastWeek)
-            .groupBy("pizza.type.value")
+        let connect = Connect(
+            projectId: "",
+            apiKey: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vYXV0aC5waGFybWFkYXRhLnRlc3QiLCJzdWIiOiJhZGFtQHBoYXJtYWRhdGEubmV0LmF1IiwiYXVkIjoiY3J5c3RhbHl6ZSIsImlhdCI6MTQ1NjgwMTQxMSwiZXhwIjoxNDU2ODA1MDExLCJuYW1lIjoiQWRhbSBHaWxtb3JlIiwicm9sZXMiOlsiRGlzcGVuc2VSZXBvcnRzLkdlbmVyaWNTdWJzdGl0dXRpb25Db250YWluZXIiLCJQb3NSZXBvcnRzLktwaURhc2hib2FyZCIsIlBvc1JlcG9ydHMuUHJvZHVjdEdyb3VwU2hhcmUiLCJEaXNwZW5zZVJlcG9ydHMuRHJ1Z1VzYWdlIiwiQWN0aXZpdHlUb29scy5BY3Rpdml0eVN0YXRlbWVudENvbnRhaW5lciIsIkFjdGl2aXR5VG9vbHMuQnVkZ2V0IiwiQWN0aXZpdHlUb29scy5IZWFsdGgiLCJEaXNwZW5zZVJlcG9ydHMuQWRob2NSZXBvcnRpbmciLCJQb3NSZXBvcnRzLk90Y0dlbmVyaWNzTWFwIiwiUG9zUmVwb3J0cy5PdGNHZW5lcmljc0NvbnRhaW5lciIsIkRpc3BlbnNlUmVwb3J0cy5HZW5lcmljRHJ1Z0xpbmVzIiwiVXNhZ2VSZXBvcnRzLlVzYWdlIiwiRGlzcGVuc2VSZXBvcnRzLlN1YnN0aXR1dGlvblBsYW4iLCJQb3NSZXBvcnRzLlByaWNlRHJvcCIsIkRpc3BlbnNlUmVwb3J0cy5QaGFybWFjeUxpc3QiLCJJbnRlcm5hbFZpZXciLCJIZWFkT2ZmaWNlSW50ZXJuYWxWaWV3IiwiTm90aWNlQm9hcmRBZG1pbiIsIlJlY29uY2lsZVJlcG9ydCIsIkVycm9yTG9ncyJdfQ.LOlW_tqIvrJX4XQe8Y1LAgAbuagvyEOO8FmZlLOnq1s",
+            baseUrl: "http://192.168.222.1:1234")
+        
+        connect.query("PharmaData")
+            .select(["scripts": .Sum("[Scripts]")])
+            .timeframe(.LastMonth)
+            .filter([ "[Pharmacy].[Pharmacy]": .Eq(256) ])
             .execute {
                 (result) in
                 switch result {
                 case .Success(let results):
                     print(results.results.first)
+                    print(results.cacheKey)
                 case .Failure(let error):
                     print(error)
                 }
-            }
-
+        }
+        
+        connect.query("PharmaData")
+            .select(["scripts": .Sum("[Scripts]")])
+            .timeframe(.LastMonth)
+            .filter([ "[Pharmacy].[Pharmacy]": .Eq(256) ])
+            .execute {
+                (result) in
+                switch result {
+                case .Success(let results):
+                    print(results.results.first)
+                    print(results.cacheKey)
+                case .Failure(let error):
+                    print(error)
+                }
+        }
     }
     
 }
